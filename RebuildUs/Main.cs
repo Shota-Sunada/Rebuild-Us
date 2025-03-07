eb21edb5649eb269403c272583af57ba8ac87e64
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-03-07 23:07:12 +0900

    TOR参考に必要なコードを移植

--- RebuildUs/Main.cs
+++ RebuildUs/Main.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using AmongUs.Data;
 using BepInEx;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
@@ -28,5 +29,26 @@ internal class RebuildUsPlugin : BasePlugin
         Harmony.PatchAll();
 
         Logger.LogInfo("\"Rebuild Us\" was completely loaded! Enjoy the modifications!");
+    }
+
+    [HarmonyPatch(typeof(StatsManager), nameof(StatsManager.AmBanned), MethodType.Getter)]
+    public static class AmBannedPatch
+    {
+        public static void Postfix(out bool __result)
+        {
+            __result = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(ChatController), nameof(ChatController.Awake))]
+    public static class ChatControllerAwakePatch
+    {
+        private static void Prefix()
+        {
+            if (!EOSManager.Instance.isKWSMinor)
+            {
+                DataManager.Settings.Multiplayer.ChatMode = InnerNet.QuickChatModes.FreeChatOrQuickChat;
+            }
+        }
     }
 }
