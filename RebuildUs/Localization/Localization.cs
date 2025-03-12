1c26487bf7e64b0e637a2e4a4e56d14f14581ca8
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-03-12 16:36:23 +0900

    翻訳機能ベース

---
+++ RebuildUs/Localization/Localization.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace RebuildUs.Localization;
+
+internal static class Localization
+{
+    private static Dictionary<SupportedLangs, Dictionary<string, string>> Translations = [];
+
+    internal static void Initialize()
+    {
+        
+    }
+
+    internal static string GetString(string key, params string[] args)
+    {
+
+    }
+}
